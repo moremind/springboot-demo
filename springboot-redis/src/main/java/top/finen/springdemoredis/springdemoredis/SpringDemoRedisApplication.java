ff4d9e9175d0f6ce0d522f13e978a4487e0c2ffb
hirCodd <hefengen@hotmail.com>
2019-10-17 16:54:29 +0800

    init project

---
+++ springboot-redis/src/main/java/top/finen/springdemoredis/springdemoredis/SpringDemoRedisApplication.java
@@ -0,0 +1,13 @@
+package top.finen.springdemoredis.springdemoredis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDemoRedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDemoRedisApplication.class, args);
+    }
+
+}
---
+++ springboot-redis/src/test/java/top/finen/springdemoredis/springdemoredis/SpringDemoRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package top.finen.springdemoredis.springdemoredis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDemoRedisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

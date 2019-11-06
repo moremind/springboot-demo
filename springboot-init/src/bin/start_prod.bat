0f228971a64751b07662dd667bedeff7fb7ef61a
hirCodd <hefengen@hotmail.com>
2019-11-06 16:38:40 +0800

    springboot-init项目启动脚本

---
+++ springboot-init/src/bin/start_prod.bat
@@ -0,0 +1,14 @@
+@echo off
+rem ======================================================================
+rem windows startup script
+rem
+rem ======================================================================
+
+
+rem startup jar
+@echo off
+set path=C:\Program Files\Java\jre1.8.0_201\bin
+@echo %path%
+javaw -jar -Xms128m -Xmx512m ../boot/xxx.jar
+
+pause

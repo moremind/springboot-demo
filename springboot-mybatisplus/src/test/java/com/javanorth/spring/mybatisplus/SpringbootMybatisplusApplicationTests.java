89040dace4b3f47ca8a77ee317a4f358e0218ca5
hirCodd <hefengen@hotmail.com>
2020-06-05 18:00:33 +0800

    提交spring-security

---
+++ springboot-mybatisplus/src/test/java/com/javanorth/spring/mybatisplus/SpringbootMybatisplusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javanorth.spring.mybatisplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMybatisplusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

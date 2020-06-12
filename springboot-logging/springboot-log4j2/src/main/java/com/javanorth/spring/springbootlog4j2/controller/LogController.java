c9b76318b654be5b11235bc4c78bdf064ff3b5a9
hirCodd <hefengen@hotmail.com>
2020-06-13 03:07:27 +0800

    整合log4j2

--- springboot-logging/springboot-log4j2/src/main/java/com/javanorth/spring/springbootlog4j2/controller/LogController.java
+++ springboot-logging/springboot-log4j2/src/main/java/com/javanorth/spring/springbootlog4j2/controller/LogController.java
@@ -1,7 +1,6 @@
 package com.javanorth.spring.springbootlog4j2.controller;
 
-import com.javanorth.spring.springbootlog4j2.util.LoggerUtil;
-import org.apache.logging.log4j.LogManager;
+import com.javanorth.spring.springbootlog4j2.util.LogUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -15,26 +14,26 @@ public class LogController {
     @GetMapping("/log")
     public void test() {
 
-        LoggerUtil.info(this.getClass(), "This is info log");
+        LogUtil.info(this.getClass(), "This is info log");
         logger.info("this is log...");
         for (int i = 0; i < 100; i++) {
-            LoggerUtil.info(LogController.class, "This is info log");
-            LoggerUtil.warn(LogController.class,"This is warning log");
-            LoggerUtil.error(LogController.class,"This is error log");
-        }
-    }
-//
-    @Scheduled(cron = "0/2 * * * * ?")
-    public void test1() {
-        for (int i = 0; i < 2; i++) {
-            LoggerUtil.info(this.getClass(),"This is info log");
-            LoggerUtil.warn(this.getClass(),"This is warning log");
-            LoggerUtil.error(this.getClass(),"This is error log");
+            LogUtil.info(LogController.class, "This is info log");
+            LogUtil.warn(LogController.class,"This is warning log");
+            LogUtil.error(LogController.class,"This is error log");
         }
     }
 
-//    public void test2() {
-//        LoggerUtil.info(this.getClass(), "this is --");
-//    }
+    /**
+     * 用于测试
+     */
+    @Scheduled(cron = "0/30 * * * * ?")
+    public void test1() {
+        for (int i = 0; i < 2; i++) {
+            LogUtil.info(this.getClass(),"This is info log");
+            LogUtil.warn(this.getClass(),"This is warning log");
+            LogUtil.error(this.getClass(),"This is error log");
+        }
+    }
+
 
 }

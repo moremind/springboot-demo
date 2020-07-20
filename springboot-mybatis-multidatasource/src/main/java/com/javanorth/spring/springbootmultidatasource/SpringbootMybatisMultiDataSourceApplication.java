3028e8020ebe485c3aa45a06650d097319c59c1a
hirCodd <hefengen@hotmail.com>
2020-07-20 15:45:56 +0800

    提交springboot整合mybatis多数据

---
+++ springboot-mybatis-multidatasource/src/main/java/com/javanorth/spring/springbootmultidatasource/SpringbootMybatisMultiDataSourceApplication.java
@@ -0,0 +1,13 @@
+package com.javanorth.spring.springbootmultidatasource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMybatisMultiDataSourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMybatisMultiDataSourceApplication.class, args);
+    }
+
+}
---
+++ springboot-mybatis-multidatasource/src/test/java/com/javanorth/spring/springbootmultidatasource/SpringbootMybatisMultidatasourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javanorth.spring.springbootmultidatasource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMybatisMultidatasourceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

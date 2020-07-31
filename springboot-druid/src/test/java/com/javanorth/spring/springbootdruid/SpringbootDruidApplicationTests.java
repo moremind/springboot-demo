87cfbb6778c0efcf9d976daf65b9ad03380a5629
hirCodd <hefengen@hotmail.com>
2020-07-31 18:10:17 +0800

    提交springboot整合druid

---
+++ springboot-druid/src/test/java/com/javanorth/spring/springbootdruid/SpringbootDruidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javanorth.spring.springbootdruid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDruidApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
